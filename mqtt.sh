976ff7e810d9b8e36eae544990330d0d7c8f8659
Stefan Heine <stheine@arcor.de>
2021-02-03 11:15:55 +0100

    Fix startup script

--- mqtt.sh
+++ mqtt.sh
@@ -2,9 +2,9 @@
 
 CHECKPID=`cat ~/.mqtt.pid 2>/dev/null`
 if [ -n "$CHECKPID" ]; then
-  kill -0 $CHECKPID
+  kill -0 $CHECKPID 2>/dev/null
   if [ $? = 0 ]; then
-    PROCESS=`ps -ef | grep -e " $CHECKPID .*mqtt.sh" | grep -v grep`
+    PROCESS=`ps -ef | grep -e " $CHECKPID .*mqtt.js" | grep -v grep`
     if [ -n "$PROCESS" ]; then
       # Already running
       exit 0
